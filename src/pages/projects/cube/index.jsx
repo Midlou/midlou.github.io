f972fb309164f4fb099e7cdf6ab6a220bb28655a
Midlou <midlou15@hotmail.com>
2022-06-14 21:35:01 -0300

    Cube testing

--- src/pages/projects/cube/index.jsx
+++ src/pages/projects/cube/index.jsx
@@ -2,8 +2,47 @@ import React, { useEffect, useRef, useState } from "react";
 import { extend, Canvas, useFrame, useThree } from '@react-three/fiber';
 import { PerspectiveCamera } from "three";
 import { OrbitControls, TransformControls } from 'three-stdlib';
+import MainLayout from "@components/layouts/main";
 
 extend({ OrbitControls, TransformControls });
+
+// const CameraController = () => {
+// 	const { camera, gl } = useThree();
+// 	useEffect(
+// 		() => {
+// 			const controls = new OrbitControls(camera, gl.domElement);
+
+// 			controls.enableDamping = true;
+// 			controls.dampingFactor = 0.05;
+// 			controls.update();
+// 			return () => {
+// 				controls.dispose();
+// 			};
+// 		},
+// 		[camera, gl]
+// 	);
+// 	return null;
+// };
+
+const CameraControls = () => {
+	const {
+		camera,
+		gl: { domElement },
+	} = useThree();
+	const controls = useRef();
+
+	let configs = {
+		ref: controls,
+		enableDamping: true,
+		dampingFactor: 0.05,
+		rotateSpeed: 0.25,
+		minZoom: 0.5,
+		maxZoom: 1,
+	}
+
+	useFrame((state) => controls.current.update());
+	return <orbitControls {...configs} args={[camera, domElement]} />;
+};
 
 const Box = (props) => {
 	// This reference will give us direct access to the mesh so we can animate it
@@ -11,27 +50,32 @@ const Box = (props) => {
 
 	// Set up state for the hovered and active state
 	const [hovered, setHover] = useState(false);
-	const [active, setActive] = useState(false);
 
 	// Rotate mesh every frame, this is outside of React without overhead
 	useFrame((state) => {
 		mesh.current.rotation.x = mesh.current.rotation.y += 0.002
 	})
 
+	let materialSettings = {
+		color: "slategrey",
+		transparent: false,
+		opacity: 1,
+		roughness: 0.01,
+		metalness: 0.95,
+	};
+
 	return (
 		<mesh
 			{...props}
 			ref={mesh}
-			scale={active ? [3, 3, 3] : [1.5, 1.5, 1.5]}
-			onClick={e => setActive(!active)}
-			onPointerOver={e => setHover(true)}
-			onPointerOut={e => setHover(false)}
+			scale={[1.5, 1.5, 1.5]}
+			// onClick={e => console.log(e)}
+			// onPointerOver={e => setHover(true)}
+			// onPointerOut={e => setHover(false)}
 		>
 			<boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-			<meshStandardMaterial
-				attach="material"
-				color={hovered ? "lightskyblue" : "lightsteelblue"}
-			/>
+			{/* <dodecahedronGeometry attach="geometry" args={[1, 0]} /> */}
+			<meshStandardMaterial attach="material" {...materialSettings} />
 		</mesh>
 	)
 }
@@ -54,14 +98,19 @@ const Sphere = (props) => {
 	)
 }
 
-function Controls() {
-	const {
-		camera,
-		gl: { domElement },
-	} = useThree();
+// function Controls() {
+// 	const {
+// 		camera,
+// 		gl: { domElement }
+// 	} = useThree();
 
-	return <orbitControls args={[camera, domElement]} />;
-}
+// 	const controls = <OrbitControls args={[camera, domElement]} />
+
+// 	controls.enableDamping = true; 
+// 	controls.dampingFactor = 0.05;
+
+// 	return controls;
+// }
 
 function CameraHelper() {
 	const camera = new PerspectiveCamera(60, 1, 1, 3);
@@ -70,22 +119,28 @@ function CameraHelper() {
 	</group>;
 }
 
-const IndexPage = () => {
-	let firstLightPos = [2, 2, 2];
-	let secondLightPos = [-1.5, -1.5, 1.5];
+const PageContent = () => {
+	let firstLightPos = [-2, 2, 2];
+	let secondLightPos = [2, -2, 2];
+	let thirdLightPos = [2, 2, -2];
 
-	return <div className="w-screen h-screen">
-		<Canvas>
-			<ambientLight />
-			<pointLight position={firstLightPos} color="red" />
-			<pointLight position={secondLightPos} color="purple" />
-			<Box position={[0, 0, 0]} />
-			<Sphere position={firstLightPos} shadow={false} />
-			<Sphere position={secondLightPos} shadow={false} />
+	return <Canvas>
+		<CameraControls />
+		<ambientLight intensity={3}/>
 
-			<Controls />
-			{/* <CameraHelper /> */}
-		</Canvas>
-	</div>
+		<pointLight position={firstLightPos} color="white" intensity={3} />
+		<pointLight position={secondLightPos} color="white" intensity={3} />
+		<pointLight position={thirdLightPos} color="white" intensity={3} />
+
+		<Box position={[0, 0, 0]} />
+		<Sphere position={firstLightPos} shadow={true} />
+		<Sphere position={secondLightPos} shadow={true} />
+		<Sphere position={thirdLightPos} shadow={true} />
+
+		{/* <CameraHelper /> */}
+	</Canvas>
+
 }
-export default IndexPage
+
+const Layout = () => <MainLayout title={"Home"} Component={<PageContent />} />;
+export default Layout;
