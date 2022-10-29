d40417971afde7b8b973974defaa3503992f4204
Midlou <midlou15@hotmail.com>
2022-10-28 22:52:13 -0300

    Added little red star project

---
+++ src/constants/little-red-star-releases.js
@@ -0,0 +1,23 @@
+export const LITTLE_RED_STAR_RELEASES = [
+	{
+		name: "b1b",
+		description: "Minor fixes and adjustments",
+		patch_items: [
+			"Minor adjustments to the lighting",
+			"Minor adjustments to scripting",
+			"Fixed missing flashlight model",
+			"Fixed nodraw, floating props",
+			"Possible fix for a softlock on generator 2",
+			"Added hint on the generator 2",
+			"Added light clues to important elements",
+		]
+	},
+	{
+		name: "b1 - Comp. release",
+		description: "Original release for the competition. I consider it as beta 1. Download includes the VMF.",
+		link: "/downloads/little-red-star-comp-release-files.zip",
+		patch_items: [
+			"Original release"
+		]
+	},
+];
---
+++ src/pages/projects/little_red_star/about/index.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+import MainLayout from "@components/layouts/main";
+
+import { LITTLE_RED_STAR_RELEASES } from "@constants/little-red-star-releases";
+import HyperLink from "@components/links/hyper-link";
+import ContainerFlex from "@components/containers/container-flex";
+import Panel from "@components/panels/panel";
+import ResponsiveText from "@components/text/responsive-text";
+import DividerLabel from "@components/dividers/divider-label";
+
+const PageContent = () => {
+	return <ContainerFlex className="px-4 flex-col mx-auto lg:w-3/4 md:max-w-4xl">
+		<DividerLabel label="Little Red Star" />
+		<div>
+			<ResponsiveText>
+				Little Red Star is a map I made for the competition
+				<HyperLink link={"https://lambdabuilds.lambdageneration.com/escape-from-black-mesa"}> LambdaBuilds: Escape From Black Mesa</HyperLink>.
+				Here you will find the patch notes of releases of the map. You can download the VMF file of the release b1.
+			</ResponsiveText>
+		</div>
+		<DividerLabel label="File releases" />
+		<Panel className="bg-gray-800 overflow-x-auto">
+			<table className="default-table">
+				<thead>
+					<tr>
+						<th className="text-left w-[25%]">Name</th>
+						<th className="text-left">Description</th>
+						<th className="text-left">Link</th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						LITTLE_RED_STAR_RELEASES.map((release, index) => {
+							return <tr key={index}>
+								<td>{release.name}</td>
+								<td>{release.description}</td>
+								<td>
+									{
+										release.link
+											? <HyperLink link={release.link} label="Download" />
+											: "-"
+									}
+								</td>
+							</tr>
+						})
+					}
+				</tbody>
+			</table>
+		</Panel>
+		<DividerLabel label="Patch notes" />
+		{
+			LITTLE_RED_STAR_RELEASES.map((release, index) => {
+				return <div key={index} className="mb-4">
+					<b>{release.name}</b>
+					<div className="flex">
+						<ul className="rounded-lg w-96 list-disc list-inside">
+							{
+								release.patch_items.map((item, index) => {
+									return <li key={index} className="px-4 w-full">
+										{
+											item
+										}
+									</li>
+								})
+							}
+						</ul>
+					</div>
+				</div>;
+			})
+		}
+	</ContainerFlex>;
+}
+
+const Layout = () => <MainLayout title={"Little Red Star"} Component={<PageContent />} />;
+export default Layout;
