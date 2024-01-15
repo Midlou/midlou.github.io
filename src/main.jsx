f363cd360653bdaa82d96c2e054354dde22ff260
Midlou <midlou15@hotmail.com>
2024-01-14 23:09:11 -0300

    Adjustments to router and deploy script

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes, } from "react-router-dom";
+import { createHashRouter, RouterProvider, } from "react-router-dom";
 
 // Pages
 import PageHome from './pages/home.jsx';
@@ -10,32 +10,24 @@ import PageProjects from './pages/projects.jsx';
 import ErrorPage from './error-page.jsx';
 import PageMiceblocks from './pages/projects/miceblocks.jsx';
 
-// const router = createBrowserRouter([
-// 	{
-// 		path: "/",
-// 		element: <PageHome />,
-// 		errorElement: <ErrorPage />,
-// 	},
-// 	{
-// 		path: "/projects",
-// 		element: <PageProjects />,
-// 	},
-// 	{
-// 		path: "/projects/miceblocks",
-// 		element: <PageMiceblocks />,
-// 	},
-// ]);
+const router = createHashRouter([
+	{
+		path: "/",
+		element: <PageHome />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "/projects",
+		element: <PageProjects />,
+	},
+	{
+		path: "/projects/miceblocks",
+		element: <PageMiceblocks />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		{/* <RouterProvider router={router} /> */}
-
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<PageHome />} />
-				<Route path="/projects" element={<PageProjects />} />
-				<Route path="/projects/miceblocks" element={<PageMiceblocks />} />
-			</Routes>
-		</BrowserRouter>
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 )
