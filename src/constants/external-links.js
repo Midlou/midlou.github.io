0c12577a870a89b1395e2b928d9b3261ae439d06
Midlou <midlou15@hotmail.com>
2024-06-23 20:06:07 -0300

    Updated index page

--- src/constants/external-links.js
+++ src/constants/external-links.js
@@ -11,14 +11,4 @@ export const EXTERNAL_LINKS = {
 		"label": "Miceblocks on Planet Minecraft",
 		"link": "https://www.planetminecraft.com/texture-pack/mice-blocks-x16/"
 	},
-	"doomInspirations": [
-		{
-			"label": "Filipe Deschamps Doom fire implementation video",
-			"link": "https://www.youtube.com/watch?v=fxm8cadCqbs"
-		},
-		{
-			"label": "Doom fire canvas implementation",
-			"link": "https://github.com/fabiensanglard/DoomFirePSX"
-		}
-	]
 }
